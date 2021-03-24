de7a38c176efc7ee96222adbddf5db8c751ac3fe
operationstratus <61917940+operationstratus@users.noreply.github.com>
2021-03-24 17:50:26 +0100

    Update HDL_generator_01.py

--- HDL_generator/HDL_generator_01.py
+++ HDL_generator/HDL_generator_01.py
@@ -14,6 +14,6 @@ for i in range(16):
 # Mux4Way16
 for i in range(16):
     print(f"//Mux4Way abcd bit {i}")
-    print(f"Mux(a=a[{i}], b=b[{i}], sel=sel[0]], out=Muxab{i});")
-    print(f"Mux(a=d[{i}], b=c[{i}], sel=sel[0]], out=Muxdc{i});")
-    print(f"Mux(a=Muxab{i}, b=Muxdc{i}, sel=sel[1]], out=out[{i}]);\n")
+    print(f"Mux(a=a[{i}], b=b[{i}], sel=sel[1], out=Muxab{i});")
+    print(f"Mux(a=d[{i}], b=c[{i}], sel=sel[1], out=Muxdc{i});")
+    print(f"Mux(a=Muxab{i}, b=Muxdc{i}, sel=sel[0], out=out[{i}]);\n")
