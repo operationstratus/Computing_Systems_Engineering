df1e3e7dd2ac5c1e133ef23e677630ffe00a2fba
operationstratus <61917940+operationstratus@users.noreply.github.com>
2021-03-24 18:50:21 +0100

    Update HDL_generator_01.py

--- HDL_generator/HDL_generator_01.py
+++ HDL_generator/HDL_generator_01.py
@@ -19,7 +19,8 @@ for i in range(16):
     print(f"Mux(a=Muxab{i}, b=Muxdc{i}, sel=sel[0], out=out[{i}]);\n")
 '''
 
+
 # Add16
-print(f"HalfAdder(a=a[0], b=b[0], sum=out[0], carry=carry0);")
+print("HalfAdder(a=a[0], b=b[0], c=false, sum=out[0], carry=carry0);")
 for i in range(1, 16):
     print(f"FullAdder(a=a[{i}], b=b[{i}], c=carry{i-1}, sum=out[{i}], carry=carry{i});")
