5162b7ada409666dc161f6af2a9a4b6d74f9d0f3
operationstratus <61917940+operationstratus@users.noreply.github.com>
2021-03-30 12:16:28 +0200

    Update HDL_generator_01.py

--- HDL_generator/HDL_generator_01.py
+++ HDL_generator/HDL_generator_01.py
@@ -44,7 +44,7 @@ inName = "in"
 outName = "DmuxOut"
 selName = "address"
 code = ""
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+#alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 for i in range(bits):
     code += f"DMux8Way(in={inName}[{i}], sel={selName}, "
@@ -56,7 +56,7 @@ for i in range(bits):
 print(code)
 '''
 
-
+'''
 #RAM8
 bits = 16
 ways = 8
@@ -77,8 +77,64 @@ code += "\n" + "//Mux8Way16 för att adressen ska kunna välja utsignalen från 
 for j in range(ways):
     code += f"{alphabet[j]}=out{j}, "
 code += "sel=address, out=out);"
-print(code)
 
+'''
+
+'''#RAM
+bits = 16
+ways = 8
+code = ""
+subchip = "RAM4K" # vårt subchips som vi stackar i chipet
+address_pins = 12
+
+code += f"//DMux8Way för att skicka 'load'-signalen till det {subchip} som ska vara mottaglig för ny data\n"
+code += "//OBS, vi delar upp 'address'-signalen i två delar:\n"
+code += f"//De första tre bitarna går till DMux8Way för att 'load'-signalen ska till rätt {subchip}\n"
+code += f"//De sista {address_pins-3} bitarna går till 'address'-pinnarna på varje {subchip}\n"
+code += "DMux8Way(in=load, sel=address[0..2], "
+for j in range(ways):
+    code += f"{alphabet[j]}=load{j}" + (not j == ways-1)*", "
+code += ");" + "\n\n"
+
+code += f"//8x {subchip} som får 'load'-signal från DMuxen\n"
+for j in range(ways):
+    code += f"{subchip}(in=in, load=load{j}, address=address[3..{address_pins-1}], out=out{j});" + "\n"
+
+
+code += "\n" + f"//Mux8Way16 för att adressen ska kunna välja utsignalen från rätt {subchip}" + "\n" + "Mux8Way16("
+for j in range(ways):
+    code += f"{alphabet[j]}=out{j}, "
+code += "sel=address[0..2], out=out);"
+
+'''
+
+'''#RAM16K
+bits = 16
+ways = 4
+code = ""
+subchip = "RAM4K" # vårt subchips som vi stackar i chipet
+address_pins = 14
+
+code += f"//DMux{ways}Way för att skicka 'load'-signalen till det {subchip} som ska vara mottaglig för ny data\n"
+code += "//OBS, vi delar upp 'address'-signalen i två delar:\n"
+code += f"//De första 2 bitarna går till DMux{ways}Way för att 'load'-signalen ska till rätt {subchip}\n"
+code += f"//De sista {address_pins-2} bitarna går till 'address'-pinnarna på varje {subchip}\n"
+code += f"DMux{ways}Way(in=load, sel=address[0..1], "
+for j in range(ways):
+    code += f"{alphabet[j]}=load{j}" + (not j == ways-1)*", "
+code += ");" + "\n\n"
+
+code += f"//{ways}x {subchip} som får 'load'-signal från DMuxen\n"
+for j in range(ways):
+    code += f"{subchip}(in=in, load=load{j}, address=address[2..{address_pins-1}], out=out{j});" + "\n"
+
+
+code += "\n" + f"//Mux{ways}Way16 för att adressen ska kunna välja utsignalen från rätt {subchip}" + "\n" + f"Mux{ways}Way16("
+for j in range(ways):
+    code += f"{alphabet[j]}=out{j}, "
+code += "sel=address[0..1], out=out);"
+'''
+print(code)
 
 
 
